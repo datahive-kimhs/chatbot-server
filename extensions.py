45fb84df156044d3b81c1fde19aa99bc96c1bad4
junseok-datahive <117891363+junseok-datahive@users.noreply.github.com>
2023-03-10 14:07:42 +0900

    update

--- extensions.py
+++ extensions.py
@@ -1,10 +1,8 @@
 from data.data import ModelDataExample
 
-
 model_data = ModelDataExample()
-
 
 def initialize():
     model_data.load_data(f'data/chatting_ckline.json', f'data/chatting_kr.json', f'data/merge_data_ja.json')
     model_data.dataset_bm25()
-    model_data.load_model()
+    # model_data.load_model()
